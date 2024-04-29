476f60b47b97486fc331056fbd666a497bbabfdc
Vivek <vr7064300@gmail.com>
2024-04-30 00:08:25 +0530

    add home route

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Please login");
 });
 
+app.get("/home", (req, res) => {
+  res.send("hi how are you");
+});
+
 app.listen(port, () => {
   console.log("Server is running on http://localhost:3000/");
 });
